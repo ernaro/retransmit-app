f8b29e6a8e813c2ec24df060971def1aa2a3f317
ernaro <askopych@ukr.net>
2022-04-21 11:21:48 +0300

    update

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://localhost/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
